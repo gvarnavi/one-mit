6a9b0dc5b077f09978990f9b31b2421e4d19f008
gvarnavi <gvarnavi@mit.edu>
2019-03-14 15:06:32 -0400

    search suggestion

--- search.php
+++ search.php
@@ -8,13 +8,17 @@
 	//get search term
 	$searchTerm = $_GET['term'];
 	$words=explode(" ",$searchTerm);
+
+	//space separate
 	$strings=array();
 	foreach($words as $substring){
 		$strings[]="fullName LIKE '%".$substring."%'";
 	}
 	$imploded=implode($strings," AND ");
-	$queryString="SELECT DISTINCT * FROM locations WHERE ".$imploded." LIMIT 50";
 
+	//perform query
+	$maxQuery=15;
+	$queryString="SELECT DISTINCT * FROM locations WHERE ".$imploded." LIMIT ".$maxQuery;
 	$query = $db->query($queryString);
 	
 	//get matched data from table
@@ -24,7 +28,16 @@
 		$row['label']=$row['fullName'];
 		$row_set[] = $row;
 	}
-    
+	if (sizeof($row_set)==$maxQuery){
+		$row['value']="";
+		$row['label']="-----------";
+		$row_set[] = $row;
+	
+		$row['value']="";
+		$row['label']="Try with more detail";
+		$row_set[] = $row;
+	}
+
 	//return json data
 	echo json_encode($row_set);
 ?>
